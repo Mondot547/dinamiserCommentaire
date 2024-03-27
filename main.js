26657d5b8003b73cd84a41b1ed631013f63535e3
Mondot547 <alex.mondot.mail.pro@gmail.com>
2024-03-27 19:27:02 +0100

    creation decoute submit form, enlever le chargement de page, recuperer les valeur inputs, creer condition pour error message et creation du new comment

--- main.js
+++ main.js
@@ -1,5 +1,37 @@
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector('form');
     const commentList = document.getElementById('comment-list');
-    
-})
+
+    form.addEventListener('submit', (event) => {
+
+        //Empeche le rechargement de la page
+        event.preventDefault();
+
+        //Recupere les valeurs des inputs
+        const firstName = document.getElementById('first-name').value;
+        const lastName = document.getElementById('last-name').value;
+        const message = document.getElementById('message').value;
+
+        //condition pour afficher le message d'error en cas d'inputs vide
+        if (firstName === '' || lastName === '' || message === '') {
+            document.getElementById('error-message').style.display = 'block';
+        } else {
+            document.getElementById('error-message').style.display = 'none';
+        }
+
+        //Créer le nouveau commentaire
+        const newComment = document.createElement('div');
+
+        //Construire le contenu du commentaire
+        newComment.innerHTML = `
+            <div class="flex space-x-4 text-sm text-gray-500">
+                <div class="flex-1 py-10 border-t border-gray-200">
+                    <h3 class="font-medium text-gray-900">${firstName} ${lastName}</h3>
+                    <div class="prose prose-sm mt-4 max-w-none text-gray-500">
+                        <p>${message}</p>
+                    </div>
+                </div>
+            </div>
+        `;
+    });
+});
